2512428d5b5b6a6dfa8db6c378327ced706bc057
aulween972 <aulweenvaldy@gmail.com>
2022-05-31 17:59:31 +0200

    first commit

---
+++ app/src/main/java/com/example/mareu/Reunion.java
@@ -0,0 +1,35 @@
+package com.example.mareu;
+
+import java.util.Date;
+
+public class Reunion {
+    private String subject;
+    private Date time;
+    private String place;
+    private String person;
+
+    Reunion(String subject, Date time, String place, String person) {
+
+        this.subject = subject;
+        this.time = time;
+        this.place = place;
+        this.person = person;
+
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+}
